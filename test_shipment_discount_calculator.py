132e864a6b08556a87f1632a2d6c5d1b5ec7b73d
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2022-06-01 15:31:28 +0300

    Minor changes

--- test_shipment_discount_calculator.py
+++ test_shipment_discount_calculator.py
@@ -1,20 +1,22 @@
-import unittest
-from shipment_discount_calculator import *
+# import unittest
+# from shipment_discount_calculator import *
+"""
+Tests not finished
+"""
 
-
-class TestShipmentDiscountCalc(unittest.TestCase):
-    def test_fl_to_str(self):
-        self.assertEqual(float_to_str(1.02985548), '1.03')
-        self.assertEqual(float_to_str(0), '0.00')
-        self.assertEqual(float_to_str('I am string'), 'I am string')
-
-    def test_la_poste(self):
-        self.assertEqual(la_poste({
-            'tr_date': ['2021', '12', '31'],
-            'size': 'L',
-            'courier': 'LP',
-
-        }), {
-            'reduced_price': 6.9,
-            'discount': '-'
-        })
+# class TestShipmentDiscountCalc(unittest.TestCase):
+#     def test_fl_to_str(self):
+#         self.assertEqual(float_to_str(1.02985548), '1.03')
+#         self.assertEqual(float_to_str(0), '0.00')
+#         self.assertEqual(float_to_str('I am string'), 'I am string')
+#
+#     def test_la_poste(self):
+#         self.assertEqual(la_poste({
+#             'tr_date': ['2021', '12', '31'],
+#             'size': 'L',
+#             'courier': 'LP',
+#
+#         }), {
+#             'reduced_price': 6.9,
+#             'discount': '-'
+#         })
